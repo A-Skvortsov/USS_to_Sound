84c27fd2309513164701083a7a885ac355e9573b
Andrey Skvortsov <andreyskv49@gmail.com>
2024-08-27 22:57:38 -0400

    General Updates
    1. changed manual computation of buzzer output to use map() function, converting distance input to buzzer output smoothly
    
    2. Cleaned up some functions for better code practice
    
    3. Added some comments
    
    Todo: (1) made LED blink when distance input is past a certain threshhold (for added effect), (2) remove delay() from loop and use some other button debounce technique as having delay() in the loop that controls I/O messes with buzzer volume transitions. (3????) add temperature & humidity sensor for better computation of speed of sound for more accurate distance computations ????

--- US_to_Sound/US_to_Sound.ino
+++ US_to_Sound/US_to_Sound.ino
@@ -17,15 +17,16 @@
 
 //logic constants
 #define SOUND_SPEED 34  //in centimeters per millisecond
-#define MAX_DISTANCE 30  //in centimeters
-#define MIN_DISTANCE 2
-#define HIGH_RANGE 5
-#define MAX_ANALOG_VAL 255
-#define DEBOUNCE 100
+#define MAX_DISTANCE 100  //in centimeters (max. distance from sensor to trigger buzzer)
+#define MIN_DISTANCE 2  //min. distance from sensor to trigger buzzer (obsolete?)
+#define HIGH_RANGE 5  //range around MIN_DISTANCE within which MAX_VOLUME should be outputted from buzzer
+#define DEBOUNCE 100  //delay for button debouncing
+#define MAX_VOLUME 255  //max. volume to be outputted from buzzer
+#define MIN_VOLUME 10  //min. volume to be outputted from buzzer (if buzzer triggered. Otherwise output 0)
 
 //global logic variables
-unsigned long uss_reading = 0;
-volatile bool on = false;
+unsigned long uss_reading = 0;  //stores time reading from ultrasonic sensor
+volatile bool on = false;  //toggles on/off of the whole I/O system
 
 
 void setup() {
@@ -43,7 +44,7 @@ void loop() {
 	delay(DEBOUNCE);
 	
 	if (on) {		
-		uss_reading = distance();
+		uss_reading = getDistance();
 		control_led(uss_reading);
 		control_buzzer(uss_reading);
 	}
@@ -58,6 +59,7 @@ void btn_press() {
 
 
 void control_led(unsigned long x) {
+	//maybe add: if (x <= MIN_DISTANCE + HIGH_RANGE) blink one time; (blinks would repeat with loop())
 	if (x <= MAX_DISTANCE) digitalWrite(LED, HIGH);
 	else digitalWrite(LED, LOW);
 
@@ -68,27 +70,22 @@ void control_led(unsigned long x) {
 void control_buzzer(unsigned long x) {
 	//"write" will be the fraction of the max analog write value (MAX_ANALOG_VAL) to write to buzzer
 	double write = 0;
-	if (x <= MIN_DISTANCE + HIGH_RANGE) write = 0.5;  //if we are in "red zone"
-	else if (x <= MAX_DISTANCE) write = (1 - ((double)x / (double)MAX_DISTANCE)) / 2;  //if not in red zone but still within range
-	else write = 0;  //if outside of range
+	if (x <= MIN_DISTANCE + HIGH_RANGE) write = MAX_VOLUME;  //if in "red zone"
+	else if (x <= MAX_DISTANCE) write = map(x, MIN_DISTANCE, MAX_DISTANCE, MAX_VOLUME, MIN_VOLUME);  //if in range
+	//note: if not in range, default write value is 0
 	
-	analogWrite(BUZZ, trunc(write * MAX_ANALOG_VAL));  //writes to buzzer appropriate fraction of max analog write value
+	analogWrite(BUZZ, write);
 }
 
 
-/* distance()
+/* getDistance()
  * Computes distance measured by ultrasonic sensor using standard [distance = speed * time] formula.
 */
-unsigned long distance() {
-	return SOUND_SPEED * get_travel_time_ms() / 2;
-}
-
-
-unsigned long get_travel_time_ms() {
+unsigned long getDistance() {
 	digitalWrite(TRANSMIT_USS, LOW);  //to clear the sensor
 	digitalWrite(TRANSMIT_USS, HIGH);
 	delayMicroseconds(10);
 	digitalWrite(TRANSMIT_USS, LOW);
 	
-	return (pulseIn(RECEIVE_USS, HIGH) / 1000);
+	return (SOUND_SPEED * pulseIn(RECEIVE_USS, HIGH) / 2000);  // div. by 1000 to convert microseconds to milliseconds, div. by 2 due to ultrasonic sensor logic (distance must be divided by 2)
 }
