1298a22f55ad15a1d9c78ba02e5145014f9c549a
Andrey Skvortsov <andreyskv49@gmail.com>
2024-08-27 20:15:06 -0400

    Added System on/off toggle
    Added a button to the circuit and used interrupt to have button toggle turn I/O loop on/off.
    
    Todo: make volume gradient instead of discrete volume levels for distance changes. Maybe do same for led brightness?

--- US_to_Sound/US_to_Sound.ino
+++ US_to_Sound/US_to_Sound.ino
@@ -5,11 +5,15 @@
  * Program to convert ultrasonic sensor input to LED output
 **/
 
+//imported libraries
+#include <math.h>
+
 //pin declarations
 #define TRANSMIT_USS 8
 #define RECEIVE_USS 7
-#define LED 2
+#define LED 4
 #define BUZZ 3
+#define BTN 2
 
 //logic constants
 #define SOUND_SPEED 34  //in centimeters per millisecond
@@ -17,24 +21,39 @@
 #define MIN_DISTANCE 2
 #define HIGH_RANGE 5
 #define MAX_ANALOG_VAL 255
+#define DEBOUNCE 100
 
 //global logic variables
 unsigned long uss_reading = 0;
+volatile bool on = false;
 
 
 void setup() {
 	pinMode(LED, OUTPUT);
 	pinMode(TRANSMIT_USS, OUTPUT);
 	pinMode(RECEIVE_USS, INPUT);
+	pinMode(BTN, INPUT);
 	
 	digitalWrite(LED, LOW);
+	attachInterrupt(digitalPinToInterrupt(BTN), btn_press, FALLING);
 }
 
 
 void loop() {
-	uss_reading = distance();
-	control_led(uss_reading);
-	control_buzzer(uss_reading);
+	delay(DEBOUNCE);
+	
+	if (on) {		
+		uss_reading = distance();
+		control_led(uss_reading);
+		control_buzzer(uss_reading);
+	}
+}
+
+
+void btn_press() {
+	on = !on;
+	digitalWrite(LED, LOW);
+	analogWrite(BUZZ, 0);
 }
 
 
@@ -49,11 +68,11 @@ void control_led(unsigned long x) {
 void control_buzzer(unsigned long x) {
 	//"write" will be the fraction of the max analog write value (MAX_ANALOG_VAL) to write to buzzer
 	double write = 0;
-	if (x <= MIN_DISTANCE + HIGH_RANGE) write = 1;  //if we are in "red zone"
-	else if (x <= MAX_DISTANCE) write = 1 - ((double)x / (double)MAX_DISTANCE);  //if not in red zone but still within range
+	if (x <= MIN_DISTANCE + HIGH_RANGE) write = 0.5;  //if we are in "red zone"
+	else if (x <= MAX_DISTANCE) write = (1 - ((double)x / (double)MAX_DISTANCE)) / 2;  //if not in red zone but still within range
 	else write = 0;  //if outside of range
 	
-	analogWrite(BUZZ, (int)(write * MAX_ANALOG_VAL));  //writes to buzzer appropriate fraction of max analog write value
+	analogWrite(BUZZ, trunc(write * MAX_ANALOG_VAL));  //writes to buzzer appropriate fraction of max analog write value
 }
 
 
